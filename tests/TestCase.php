a9609ef2c7cc870a6a142b09c9e4371b2ddf4b13
higorldmoreira <higorldmoreira@gmail.com>
2026-02-23 00:43:22 -0300

    refactor: remove createApplication method from TestCase class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,14 +8,5 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    public function createApplication(): \Illuminate\Foundation\Application
-    {
-        $app = parent::createApplication();
-
-        $app['config']->set('database.default', 'sqlite');
-        $app['config']->set('database.connections.sqlite.database', ':memory:');
-        $app['config']->set('cache.default', 'array');
-
-        return $app;
-    }
+    //
 }
